a6ee55825a2591843c5e68e49d762867cc597e32
Benjamin Pelc <bjpelcdev@outlook.com>
2015-03-18 18:54:55 +0000

    Set up grid tests for rectangular image

--- tests/grid_tests.cpp
+++ tests/grid_tests.cpp
@@ -57,6 +57,52 @@ TEST_CASE( "Grid: Square Image"){
 		CHECK(coordsVec[2364].first == 271);
 		CHECK(coordsVec[2364].second == 303);
 	}
+}
+
+TEST_CASE( "Grid: Rectangular Image"){
+	
+	/* 512 x 256 test image, 16 x 16 pixel windows with 8 pixel overlap */
+
+	PivOptions::Uptr opt(new PivOptions());
+	IntMap::Uptr im(new IntMap(512, 256));
+
+	Grid g(opt, im);
+
+	SECTION( "Number of x grid points should equal 31") {
+		CHECK(g.get_noPointsX() == 31);
+	}
+
+	SECTION( "Number of y grid points should equal 63") {
+		CHECK(g.get_noPointsY() == 63);
+	}
+
+	SECTION( "Length of x coordinate vector should equal 63" ) {
+		CHECK(g.get_xCoordVector().size() == 31);
+	}
+	
+	SECTION( "Length of y coordinate vector should equal 63" ) {
+		CHECK(g.get_yCoordVector().size() == 63);
+	}
+	
+	SECTION( "Total grid points should equal 1956" ) {
+		CHECK(g.get_totalGridPoints() == 1953);
+	}
+	
+	auto coordsVec = g.get_coordPairsVector();
+
+	SECTION( "Coord pair vector should have size 1953" ) {
+		CHECK(coordsVec.size() == 1953);
+	}
+	
+	SECTION( "First coords should be (7, 7)" ) {
+		CHECK(coordsVec[0].first == 7);
+		CHECK(coordsVec[0].second == 7);
+	}
+	
+	SECTION( "Point 273  should be (7, 7)" ) {
+		CHECK(coordsVec[273].first == 207);
+		CHECK(coordsVec[272].second == 71);
+	}
 	
 
 }
