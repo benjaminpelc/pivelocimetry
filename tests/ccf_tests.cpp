50887dcdbd6bb461707c3ac445634335f468f57b
Benjamin Pelc <bjpelcdev@outlook.com>
2015-03-19 10:42:08 +0000

    Add ccf initialize, set values and peak find tests

--- tests/ccf_tests.cpp
+++ tests/ccf_tests.cpp
@@ -2,22 +2,67 @@
 #include "catch.hpp"
 
 #include "../src/ccf.h"
+// #include <algorithm>
 
 TEST_CASE( "Test CCF", "Initialisation"){
 	
-	CCF c(9, 9);	
+	CCF c(17, 17);	
 	
 	SECTION( "All elements should be equal to -1.0" ) {
-		CHECK(c.getElem(1) == -1.0);
+		std::for_each(c.begin(), c.end(), [](auto el) { 
+				CHECK(el == -1.0);
+			});
 	}
+}
+
+TEST_CASE( "Set elements", "Use setElem to set correlation values")	
+{
+	CCF c(17, 17);	
+
+	c.setElem(2, 4, 0.998);
+	c.setElem(5, 14, 0.810);
+	c.setElem(13, 9, 0.321);
 
 	SECTION( "Set some peaks and check values" ) {
-		c.setElem(2, 4, 0.998);
-		c.setElem(4, 3, 0.810);
-		c.setElem(7, 3, 0.321);
-
 		REQUIRE(c.getElem(2, 4) == 0.998);
-		REQUIRE(c.getElem(4, 3) == 0.810);
-		REQUIRE(c.getElem(7, 3) == 0.321);
+		REQUIRE(c.getElem(5, 14) == 0.810);
+		REQUIRE(c.getElem(13, 9) == 0.321);
 	}
+}
+
+
+TEST_CASE( "Peak finder", "Find peaks")	
+{
+	CCF c(17, 17);	
+	Peak::PeaksVec ps(3);
+
+	c.setElem(2, 4, 0.998);
+	c.setElem(5, 14, 0.810);
+	c.setElem(13, 9, 0.321);
+
+	c.findPeaks(ps, 7);
+
+	Peak p1 = ps[0];
+	Peak p2 = ps[1];
+	Peak p3 = ps[2];
+
+	SECTION( "First peak should be 0.998" ) {
+		CHECK(p1.val() == 0.998 );
+	}
+
+	SECTION( "First peak should have coords (i, j) = (4, 2)" ) {
+		CHECK(p1.get_iCoord() == 4);
+		CHECK(p1.get_jCoord() == 2);
+	}
+	
+	SECTION( "Second peak should have coords (i, j) = (14, 5)" ) {
+		CHECK(p2.get_iCoord() == 14);
+		CHECK(p2.get_jCoord() == 5);
+	}
+	
+	SECTION( "Third peak should have coords (i, j) = (9, 13)" ) {
+		CHECK(p3.get_iCoord() == 9);
+		CHECK(p3.get_jCoord() == 13);
+	}
+
 }
