b53af739373156a6c4b34c599c14d7e5f195009e
Benjamin Pelc <bjpelcdev@outlook.com>
2015-03-23 09:48:25 +0000

    Modify main.cpp comments

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,14 @@
 #include "Grid.hpp"
 #include "IntMap.hpp"
 #include "OcvImage.hpp"
-#include "PivOptions.hpp"
 #include "PivClap.hpp"
-#include <memory>
+#include "PivOptions.hpp"
 #include <cmath>
+#include <memory>
 
 int main(int argc, char** argv)
 {
-	/* Arguments 
-	 * Do all parsing up front and raise exceptions if something is wrong
-	 * and end the program.
-	 *
-	 * No point doing any PIV if say write output to file is selected but no
-	 * filename is provided */
+	/* Parse command line arguments */ 
 	PivClap clArgs(argc, argv);
 
 	/* Load a raw images and extract pixel intensity maps */
@@ -26,8 +21,7 @@ int main(int argc, char** argv)
 	IntMap::Uptr i1 = std::make_unique<IntMap>(rawIm1),
 	             i2 = std::make_unique<IntMap>(rawIm2);
 
-	/* Let us try and box the images into a pair
-	 * In effect, a pair of memory addresses to the two images?? */
+	/* Box images in a pair for easy handling */
 	IntMap::Pair imPair{i1, i2};
 
 	/* Load options from config file
@@ -41,7 +35,7 @@ int main(int argc, char** argv)
 	/* We have options, images and a grid, now do some PIV */
 	PivEng::DoPiv p = PivEng::DoPiv(*analysisOptions, imPair, *g);
 	
-	/* if -o flag is supplied with a filename, write to it */
+	/* Check command line args and print to screen/write to file as necessary */
 	if (clArgs.hasParam("-o")) p.write(clArgs.getParam("-o"));
 	if (clArgs.printResults()) p.print();
 
