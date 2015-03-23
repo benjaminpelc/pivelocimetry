349a1c6042e20d6064030c7136e56e93a9e38d3d
Benjamin Pelc <bjpelcdev@outlook.com>
2015-03-23 11:26:52 +0000

    Split Clap

--- lib/Clap.hpp
+++ lib/Clap.hpp
@@ -1,10 +1,11 @@
 #ifndef COMMAND_LINE_ARGUMENT_PARSER_H
 #define COMMAND_LINE_ARGUMENT_PARSER_H
 
-#include <iostream>
 #include <algorithm>
-#include <string>
+#include <iostream>
+#include <map>
 #include <set>
+#include <string>
 
 class Clap
 {
@@ -14,14 +15,14 @@ class Clap
 		Clap(int argc, char** argv);
 		~Clap() {};
 
-		char** begin() { return &m_argv[0]; };
-		char** end() { return &m_argv[m_argc]; };
+		char** begin();
+		char** end();
 	
 		bool hasFlag(const std::string flag);
 		bool hasParam(const std::string flag);
 
-		int numArgs() { return m_argc; };
-		std::string progName() { return m_argv[0]; };
+		int numArgs();
+		std::string progName();
 
 		std::string getParam(const std::string flag);
 
@@ -36,87 +37,10 @@ class Clap
 		char** m_argv;
 
 		void getFlags();
-		static bool isFlag(const std::string a) { return a[0] == '-'; };
+		static bool isFlag(const std::string a);
 
 		FlagArgsMap m_flagArgsMap;
 
 };
-
-Clap::Clap(int argc, char** argv) :
-	m_flags(),
-	m_argc(argc),
-	m_argv(argv),
-	m_flagArgsMap()
-{
-	getFlags();
-	std::for_each(m_flags.begin(), m_flags.end(), [&](auto &s) {
-				this->m_flagArgsMap[s] = this->flagArgs(s);
-			});	
-}
-
-std::set<std::string>& Clap::flagSet()
-{
-	return m_flags;
-}
-
-Clap::FlagArgsMap& Clap::getFlagsWithArgs()
-{
-	return m_flagArgsMap;
-}
-
-std::vector<std::string> Clap::flagArgs(const std::string flag)
-{
-	std::vector<std::string> flagArgsVec;
-	
-	auto ittr = std::find(begin(), end(), flag) + 1;
-
-	std::find_if(ittr, end(), [&flagArgsVec](auto s) { 
-			if (isFlag(s)) return true; 
-			else flagArgsVec.push_back(s); 
-			return false; 
-		});
-	
-	return flagArgsVec;
-}
-
-void Clap::getFlags() {
-	std::for_each(begin(), end(), [&](auto &a) {
-				if (isFlag(a)) m_flags.insert(a);
-			});
-}
-
-bool Clap::hasFlag(const std::string flag)
-{
-	/* Returns true if args contain charFlag */
-	return std::find(begin(), end(), flag) != end();
-}
-
-bool Clap::hasParam(const std::string flag)
-{
-	return std::adjacent_find(begin(), end(), [&flag](auto &s1, auto &s2) {
-				return s1 == flag && s2[0] != '-';
-			}) != end();
-}
-
-std::string Clap::getParam(const std::string flag)
-{
-	/* adjacent_find returns a pointer to the first of two elements that meet 
-	 * the condition given by the lambda, in this case that the second string 
-	 * does not have '-' as its first character. 
-	 *
-	 * To return the second of the adjacent elements, advance the pointer
-	 * by one 
-	 *
-	 * Of course, we first need to check if the flag is present with an 
-	 * accompanying param. If no, return empty string */
-	if (hasParam(flag)) {
-		return *(std::adjacent_find(begin(), end(), [&flag](auto &s1, auto &s2) {
-						return s1 == flag && s2[0] != '-';
-					}) + 1);
-	}
-	return "";
-}
-
-
 
 #endif
---
+++ src/SubPix.cpp
@@ -0,0 +1,57 @@
+#include "SubPix.hpp"
+
+namespace PivEng {
+
+	void SubPixel::gauss(CCF& ccf, Peak::PeaksVec& peaks, Disp::DispVec& displacements)
+	{
+		// std::cout << "In Gauss, eww!" << std::endl;
+		int i, j;
+		double dx, dy;
+
+		/* Offset value make central row and column the zero displacement */
+		int cols = ccf.cols(),
+			offsetX = ccf.offsetX(),
+			offsetY = ccf.offsetY();
+
+		auto e0  = ccf.begin(),
+		 	 e = e0;
+
+		/* Calculate displacements for each peak */
+		auto p = peaks.begin();
+		auto d = displacements.begin();
+
+		while ( p->valid && p != peaks.end() ){
+			i = p->i;
+			j = p->j;
+
+			/* Pointer to correlation peak value */
+			e  = e0 + j * cols + i;
+			// std::cout << e << "\t" << *e << std::endl;
+			/* Calculate x and y subpixel displacements */
+			dx = i + gauss3((e - 1), e, (e + 1) ) - offsetX; 
+			dy = j + gauss3((e - cols), e, (e + cols)) - offsetY; 
+
+			/* A number is only not equal to itself iff it is a NaN. If the peak is invalid, 
+		 	 * assume subsequent peaks are also invalid. Invalidity is caused by taking the 
+		 	 * logarithm of a negative correlation function element. Is it logical to think
+		 	 * if a peak is invalid then all subsequent peaks will be invalid? 
+		 	 */
+			if ( dx != dx || dy != dy ) break;
+			/* put the necessary bits in */
+			d->setDisp(dx, dy, true);
+
+			/* Advance the peak and displacement vector iterators */
+			p++, d++;
+		}
+	}
+
+	// double SubPixel::gauss3(const double a, const double b, const double c)
+	// {
+	// 	return  (log(a) - log(c)) / ( 2*log(a) - 4*log(b) + 2*log(c)); 
+	// }
+
+	double SubPixel::gauss3(const double* a, const double* b, const double* c)
+	{
+		return  (log(*a) - log(*c)) / ( 2*log(*a) - 4*log(*b) + 2*log(*c)); 
+	}
+}
