fbe3701b94d1d0c8ef85d82276876af8d72801c3
Benjamin Pelc <bjpelcdev@outlook.com>
2015-03-23 10:11:59 +0000

    Ccf split

--- src/Ccf.cpp
+++ src/Ccf.cpp
@@ -1,36 +1,7 @@
-/* Class CCF 
- * Correlation function matrix for storing two dimensional cross-
- * correlation values. 
- * Inherits from Matrix2<double>
- * Includes method for locating peaks */
-
-#ifndef CCF_H
-#define CCF_H
-
-#include "../lib/Mat2.hpp"
-#include "Peak.hpp"
+#include "Ccf.hpp"
 
 namespace PivEng {
 
-	class CCF : public Mat2<double>
-	{
-		public:
-			typedef std::shared_ptr<CCF> Sptr;
-
-			CCF(unsigned int rows, unsigned int cols);
-			~CCF();
-
-			void findPeaks(Peak::PeaksVec& pv, int maxDisp);
-			bool isLocalPeak(int j);
-			int offsetX();
-			int offsetY();
-
-		private:	
-			const double BIG_DOUBLE = 9999999999.9;
-			bool inRange(double subject, double minimum, double maximum);
-			const int m_offsetX,
-			  	  	  m_offsetY;
-	};
 
 	/* Constructor:
  	 * Must be passed with row and column number specifiers.
@@ -158,5 +129,3 @@ namespace PivEng {
 	}
 
 }
-
-#endif
