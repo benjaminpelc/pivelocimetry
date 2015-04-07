911d1ba8a454caaefeed0a195c4a97aec75d6b92
Benjamin Pelc <bjpelcdev@outlook.com>
2015-04-07 23:04:36 +0100

    Move graphics to members

--- src/PivView.cpp
+++ src/PivView.cpp
@@ -1,58 +1,34 @@
 #include "PivView.hpp"
 
-// class AxisBox : public sf::VertexArray
-// {
-// 	public:
-// 		AxisBox(int x0, int x1, int y0, int y1);
-// };
-//
-// AxisBox::AxisBox(int x0, int x1, int y0, int y1) : sf::VertexArray(sf::LinesStrip, 5)
-// {
-// 	std::cout << x0 << y0 << x1 << y1 << std::endl;
-// 	std::cout << getVertexCount() << std::endl;
-// }
-
 PivView::PivView(PivEng::PivPoint::PivPointVec& vs) :
 	m_pivPointVec(vs),
 	piv_vectors(vs.size())
 {
 	get_piv_vectors_and_maximum_velocity();
 
-	std::vector<sf::VertexArray> vel_vector_graphics(num_vectors);
-	auto vel_vector_graphicsPtr = &vel_vector_graphics[0];
+	// std::vector<sf::VertexArray> vel_vector_graphics(num_vectors);
+	vel_vector_graphics.resize(num_vectors);
+	auto vel_vector_graphic = vel_vector_graphics.begin();
 
 	sf::ContextSettings settings;
 	sf::RenderWindow window(sf::VideoMode(612, 612), "BPPIV Vector Viewer", sf::Style::Default, settings);
 	window.setFramerateLimit(60);
 
-	/* Set up the axis */
-	sf::VertexArray ln(sf::Lines, 2);
+	axis_box = make_axis_box(50, 50, 562, 562);
+	crosshairs = make_crosshairs(50, 50, 562, 562);
 
-	auto axisBox = make_axis_box(50, 50, 562, 562);
-
-	auto xCross = sf::VertexArray(sf::Lines, 2);
-	auto yCross = sf::VertexArray(sf::Lines, 2);
-	xCross[0].position.x = 50;
-	xCross[1].position.x = 562;
-	yCross[0].position.y = 50;
-	yCross[1].position.y = 562;
-
-	std::for_each(m_pivPointVec.begin(), m_pivPointVec.end(), [&](auto& pointf) {
-		auto point = pointf.get_piv_vector();
-		if (point) {
-			*(vel_vector_graphicsPtr++) = make_vector_graphic(*point);	
-		}
-	});
+	for (auto& piv_vector : piv_vectors) {
+		*(vel_vector_graphic++) = make_vector_graphic(piv_vector);
+	}
 
 	/* Clear, draw, display frame rendering routine */
-	auto renderFrame = [&]() {
-		window.clear(sf::Color::Black);
-		window.draw(axisBox);
-		window.draw(xCross);
-		window.draw(yCross);
-		std::for_each(vel_vector_graphics.begin(), vel_vector_graphics.end(), [&](auto& dv) { window.draw(dv); });
-		window.display();
-	};
+	// auto renderFrame = [&]() {
+	// 	window.clear(sf::Color::Black);
+	// 	window.draw(axis_box);
+	// 	window.draw(crosshairs);
+	// 	std::for_each(vel_vector_graphics.begin(), vel_vector_graphics.end(), [&](auto& dv) { window.draw(dv); });
+	// 	window.display();
+	// };
 
 	auto mouse = sf::Vector2i();
 
@@ -65,23 +41,20 @@ PivView::PivView(PivEng::PivPoint::PivPointVec& vs) :
 					window.close();
 
 				case sf::Event::Resized:
-					renderFrame();
+					render_frame(window);
 
 				case sf::Event::MouseMoved:
 					mouse = sf::Mouse::getPosition(window);
 					if (mouse.x > 50 && mouse.x < 562 && mouse.y > 50 && mouse.y < 562) {
-						xCross[0].position.y = mouse.y;
-						xCross[1].position.y = mouse.y;
-						yCross[0].position.x = mouse.x;
-						yCross[1].position.x = mouse.x;
-						renderFrame();
+						update_crosshairs(mouse);
+						render_frame(window);
 					}
 
 				default:
 					;
 			}
 		}
-		renderFrame();
+		render_frame(window);
 	}
 }
 
@@ -108,9 +81,23 @@ void PivView::get_piv_vectors_and_maximum_velocity()
 		
 sf::Color PivView::vector_color(const double vel_magnitude, const double max_vel_magnitude) const
 {
-			auto red = 255 - static_cast<uint8_t>(vel_magnitude / max_vel_magnitude * 255);
-			auto green = static_cast<uint8_t>(0);
-			auto blue = static_cast<uint8_t>(vel_magnitude / max_vel_magnitude * 255);
+			auto magnitude_ratio = vel_magnitude / max_vel_magnitude;
+			uint8_t red, green, blue;
+			
+			red   = 255 - static_cast<uint8_t>(magnitude_ratio * 255);
+			green = static_cast<uint8_t>(0);
+			blue  = static_cast<uint8_t>(magnitude_ratio * 255);
+
+			// if (magnitude_ratio < 0.5) {
+			// 	red = 255 - static_cast<uint8_t>(2 * magnitude_ratio * 255);
+			// 	green = static_cast<uint8_t>(2 * magnitude_ratio * 255);
+			// 	blue = static_cast<uint8_t>(0);
+			// } else {
+			// 	red = 255 - static_cast<uint8_t>(magnitude_ratio * 255);
+			// 	green = static_cast<uint8_t>(0);
+			// 	blue = static_cast<uint8_t>(magnitude_ratio * 255);
+			// }
+
 			return sf::Color(red, green, blue);
 }
 
@@ -146,3 +133,41 @@ sf::VertexArray PivView::make_axis_box(const int i_min, const int j_min, const i
 	axis_box[4] = axis_box[0];
 	return axis_box;
 }
+
+sf::VertexArray PivView::make_crosshairs(const int i_min, const int j_min, const int i_max, const int j_max) const
+{	
+	sf::VertexArray crosshairs(sf::Lines, 4);
+
+	crosshairs[0].position.x = i_min;
+	crosshairs[0].position.y = j_min;
+
+	crosshairs[1].position.x = i_max;
+	crosshairs[1].position.y = j_min;
+
+	crosshairs[2].position.x = i_min;
+	crosshairs[2].position.y = j_min;
+
+	crosshairs[3].position.x = i_min;
+	crosshairs[3].position.y = j_max;
+
+	return crosshairs;
+
+}
+
+void PivView::update_crosshairs(const sf::Vector2i& mouse)
+{
+	crosshairs[0].position.y = mouse.y;
+	crosshairs[1].position.y = mouse.y;
+	crosshairs[2].position.x = mouse.x;
+	crosshairs[3].position.x = mouse.x;
+}
+
+void PivView::render_frame(sf::RenderWindow& window) const
+{
+	window.clear(sf::Color::Black);
+	window.draw(axis_box);
+	window.draw(crosshairs);
+	std::for_each(vel_vector_graphics.begin(), vel_vector_graphics.end(), [&](auto& dv) { window.draw(dv); });
+	window.display();
+}
+
