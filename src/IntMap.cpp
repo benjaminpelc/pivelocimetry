bc9bbed80cb14facea0e7973c408dd1270d8c80d
Benjamin Pelc <bjpelcdev@outlook.com>
2015-04-12 09:49:31 +0100

    Take height out of loop in IntMap

--- src/IntMap.cpp
+++ src/IntMap.cpp
@@ -5,8 +5,9 @@ IntMap::IntMap(unsigned int rows, unsigned int cols) : Mat2<uint16_t>(rows, cols
 IntMap::IntMap(IntMappable* raw) : Mat2<uint16_t>(raw->height(), raw->width())
 {
 	unsigned int wdth = raw->width();
+	unsigned int height = raw->height();
 
-	for (size_t j = 0; j < raw->height(); j++) {
+	for (size_t j = 0; j < height; j++) {
 		for (size_t i = 0; i < wdth; i++) {
 			setElem((j * wdth) +  i, raw->pixelInt(j, i));
 		}
@@ -15,13 +16,12 @@ IntMap::IntMap(IntMappable* raw) : Mat2<uint16_t>(raw->height(), raw->width())
 
 IntMap::IntMap(const std::unique_ptr<IntMappable>& raw) : Mat2<uint16_t>(raw->height(), raw->width())
 {
-	unsigned int wdth = raw->width();
-
+	const size_t wdth = raw->width();
+	const size_t height = raw->height();
 	auto fe = begin();
 	
-	for (size_t j = 0; j < raw->height(); j++) {
+	for (size_t j = 0; j < height; j++) {
 		for (size_t i = 0; i < wdth; i++) {
-			// setElem(j*wdth + i, raw->pixelInt(j, i));
 			*(fe++) =  raw->pixelInt(j, i);
 		}
 	}
